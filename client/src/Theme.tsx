9d9492d933d6cb629f8ffb8c6014a4f605e21464
Janet Yu <janettyu99@gmail.com>
2022-12-06 21:17:29 -0800

    fix primary purple hexcode

--- client/src/Theme.tsx
+++ client/src/Theme.tsx
@@ -3,14 +3,14 @@ import { ThemeProvider } from 'styled-components';
 
 const theme = {
   colors: {
-    primaryPurple: '#6974D4',
+    primaryPurple: '#4150DC',
     paleYellow: '#FFF5C1',
     grey: '#515151',
   },
 };
 
-const Theme = ({ children }) => {
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+const Theme = ({ children }: { children: JSX.Element }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
 
 export { Theme, theme };

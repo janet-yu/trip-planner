30e5dc8ba11c405dab51f71f45e1ad3a46adb626
Janet Yu <janettyu99@gmail.com>
2022-12-16 21:26:55 -0800

    remove FontStyles component and directly link google fonts in index.html

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import FontStyles from './fontStyles';
-
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <FontStyles />
     <App />
   </React.StrictMode>
 );

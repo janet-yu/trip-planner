5e40e01e204a91b35d763468f04c9ea92c35ffea
Janet Yu <janettyu99@gmail.com>
2022-12-16 21:26:00 -0800

    remove FontStyles component and directly link google fonts in index.html

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../src/Theme';
-import FontStyles from '../src/fontStyles';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },

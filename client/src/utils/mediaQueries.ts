39f2c47472cffad4db6dbea3abdc109b65078d5e
Janet Yu <janettyu99@gmail.com>
2022-12-18 11:02:56 -0800

    refactor: add media queries

---
+++ client/src/utils/mediaQueries.ts
@@ -0,0 +1,20 @@
+const screenSizes = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
+};
+
+export const device = {
+  mobileS: `(min-width: ${screenSizes.mobileS})`,
+  mobileM: `(min-width: ${screenSizes.mobileM})`,
+  mobileL: `(min-width: ${screenSizes.mobileL})`,
+  tablet: `(min-width: ${screenSizes.tablet})`,
+  laptop: `(min-width: ${screenSizes.laptop})`,
+  laptopL: `(min-width: ${screenSizes.laptopL})`,
+  desktop: `(min-width: ${screenSizes.desktop})`,
+  desktopL: `(min-width: ${screenSizes.desktop})`,
+};

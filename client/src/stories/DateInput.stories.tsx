d12b6fe5ab83b6c6796ad2bb1c9bcd35a3bfa41f
Janet Yu <janettyu99@gmail.com>
2022-12-23 18:05:58 -0800

    refactor: add date input story

---
+++ client/src/stories/DateInput.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { default as DateInput } from '../components/DatePicker/Input';
+
+export default {
+  title: 'DateInput',
+  component: DateInput,
+};
+
+export const DateInputComponent = () => <DateInput />;

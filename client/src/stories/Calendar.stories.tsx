ca20f92dd637769d41f3c6688329ac577c2f920a
Janet Yu <janettyu99@gmail.com>
2022-12-24 20:42:38 -0800

    refactor: delete Calendar story

--- client/src/stories/Calendar.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Calendar from '../components/DatePicker/Calendar';
-
-export default {
-  title: 'Calendar',
-  component: Calendar,
-};
-
-export const CalendarDefault = () => <Calendar />;

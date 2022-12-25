53ff8ec986b0bebfe442886c3d276462e2dd7e71
Janet Yu <janettyu99@gmail.com>
2022-12-24 20:37:32 -0800

    refactor: rename date picker story

--- client/src/stories/DateInput.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { default as DatePicker } from "../components/DatePicker";
-
-export default {
-  title: "DateInput",
-  component: DatePicker,
-};
-
-export const DateInputComponent = () => <DatePicker />;
---
+++ client/src/stories/DatePicker.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { default as DatePickerComponent } from "../components/DatePicker";
+
+export default {
+  title: "DatePicker",
+  component: DatePickerComponent,
+};
+
+export const DatePicker = () => <DatePickerComponent />;

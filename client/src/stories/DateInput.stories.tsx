45de9f93d55a756c882cf7dc36ca74cc2962936b
Janet Yu <janettyu99@gmail.com>
2022-12-24 16:36:40 -0800

    fix: fix import file path for DateInput

--- client/src/stories/DateInput.stories.tsx
+++ client/src/stories/DateInput.stories.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { default as DateInput } from '../components/DatePicker/Input';
+import React from "react";
+import { default as DateInput } from "../components/DatePicker/DateInput";
 
 export default {
-  title: 'DateInput',
+  title: "DateInput",
   component: DateInput,
 };
 

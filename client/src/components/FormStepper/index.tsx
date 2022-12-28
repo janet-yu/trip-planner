3265d7778ed12b872ca5bc22551c0ab40fc30e4c
Janet Yu <janettyu99@gmail.com>
2022-12-27 21:57:36 -0800

    refactor: add FormStepper index file

---
+++ client/src/components/FormStepper/index.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import styled from 'styled-components';
+import FormStep from './FormStep';
+import { device } from '../../utils/mediaQueries';
+
+const Header = styled.nav<{ stepCount: number }>`
+  padding: 1em;
+  display: grid;
+  grid-template-columns: repeat(${(props) => props.stepCount * 2}, 1fr);
+  position: relative;
+`;
+
+const ProgressBarWrapper = styled.div<{ stepCount: number }>`
+  height: 3px;
+  background-color: #eee;
+  grid-column: 2 / ${(props) => props.stepCount * 2};
+  grid-row: 1 / -1;
+  top: 1.5rem;
+  position: relative;
+  display: none;
+
+  @media ${device.mobileL} {
+    display: block;
+  }
+`;
+
+const ProgressBar = styled.div<{ stepCount: number; activeStep: number }>`
+  width: calc(
+    100% / ${(props) => props.stepCount - 1} * ${(props) => props.activeStep}
+  );
+  height: 3px;
+  background-color: ${(props) => props.theme.colors.primary['800']};
+`;
+
+type FormProgressHeaderProps = {
+  steps: {
+    label: string;
+  }[];
+  activeStep: number;
+};
+
+/**
+ *
+ * @param props
+ */
+
+// Shoutout to https://www.telerik.com/kendo-react-ui/components/form/multi-step-form/
+// for help!
+const FormProgressHeader = (props: FormProgressHeaderProps) => {
+  return (
+    <Header stepCount={props.steps.length}>
+      <ol
+        style={{
+          gridColumn: '1 / -1',
+          display: 'flex',
+          position: 'relative',
+          zIndex: '1',
+        }}
+      >
+        {props.steps.map((step, idx) => (
+          <FormStep
+            active={props.activeStep === idx}
+            completed={props.activeStep > idx}
+            label={step.label}
+            stepNumber={idx + 1}
+          />
+        ))}
+      </ol>
+      <ProgressBarWrapper stepCount={props.steps.length}>
+        <ProgressBar
+          stepCount={props.steps.length}
+          activeStep={props.activeStep}
+        ></ProgressBar>
+      </ProgressBarWrapper>
+    </Header>
+  );
+};
+
+export default FormProgressHeader;

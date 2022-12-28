aeac4a3c2b2b8fcf498520e111b156f9172df8da
Janet Yu <janettyu99@gmail.com>
2022-12-27 21:48:16 -0800

    refactor: fixing formatting and types

--- client/src/components/DatePicker/DateInput.tsx
+++ client/src/components/DatePicker/DateInput.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
-import styled, { css } from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendar } from "@fortawesome/free-solid-svg-icons";
-import * as dateHelpers from "../../utils/dateHelpers";
+import React, { useEffect, useRef, useState } from 'react';
+import styled, { css } from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+import * as dateHelpers from '../../utils/dateHelpers';
 
 const DateInputContainer = styled.div<{ focused?: boolean }>`
   display: flex;
   align-items: center;
   border: 2px solid transparent;
-  border-bottom: 2px solid ${(props) => props.theme.colors.primary["800"]};
+  border-bottom: 2px solid ${(props) => props.theme.colors.primary['800']};
   border-radius: 0.2rem;
   max-width: 200px;
   position: relative;
@@ -16,24 +16,24 @@ const DateInputContainer = styled.div<{ focused?: boolean }>`
   ${(props) =>
     props.focused &&
     css`
-      border: 2px solid ${(props) => props.theme.colors.primary["800"]};
+      border: 2px solid ${(props) => props.theme.colors.primary['800']};
     `}
 `;
 
 const CalendarButton = styled.button`
   width: 32px;
   height: 32px;
-  color: ${(props) => props.theme.colors.primary["800"]};
+  color: ${(props) => props.theme.colors.primary['800']};
   border-radius: 100%;
   &:hover {
-    background: ${(props) => props.theme.colors.primary["50"]};
+    background: ${(props) => props.theme.colors.primary['50']};
     cursor: pointer;
   }
 `;
 
 const Input = styled.input`
   border: none;
-  color: ${(props) => props.theme.colors.primary["800"]};
+  color: ${(props) => props.theme.colors.primary['800']};
   font-size: 1rem;
   padding: 0.8rem;
   flex: 1;
@@ -70,7 +70,7 @@ const DateInput = (props: any) => {
     setInputFocused(true);
   };
 
-  const handleChange = (evt) => {
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
     let caretStart = evt.target.selectionStart;
     let caretEnd = evt.target.selectionEnd;
 
@@ -80,27 +80,30 @@ const DateInput = (props: any) => {
 
     // 1. Remove any non numeric input
     const regex = /[^0-9]+/gi;
-    const sanitizedValue = currentValue.replace(regex, "");
+    const sanitizedValue = currentValue.replace(regex, '');
 
     // Move everything to the right every time we input a number
     // Place slashes in the correct place
-    let valueSplit = sanitizedValue.split("");
+    let valueSplit = sanitizedValue.split('');
 
     if (valueSplit.length >= 2) {
-      valueSplit.splice(2, 0, "/");
+      valueSplit.splice(2, 0, '/');
     }
 
     if (valueSplit.length > 4) {
-      valueSplit.splice(5, 0, "/");
+      valueSplit.splice(5, 0, '/');
     }
 
-    const finalValue = valueSplit.join("").substring(0, 10);
+    const finalValue = valueSplit.join('').substring(0, 10);
 
     setInputValue(finalValue);
-    setCursor({
-      start: caretStart,
-      end: caretEnd,
-    });
+
+    if (caretStart && caretEnd) {
+      setCursor({
+        start: caretStart,
+        end: caretEnd,
+      });
+    }
 
     // If we have a valid date, update the selected date
     const constructedDate = new Date(finalValue);
@@ -129,7 +132,7 @@ const DateInput = (props: any) => {
         onFocus={onInputFocus}
         onBlur={onInputBlur}
         onChange={handleChange}
-        pattern={"[0-9]"}
+        pattern={'[0-9]'}
         ref={ref}
       />
     </DateInputContainer>

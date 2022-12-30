0ae7b422600fed462f261540d8750cddf0b17db2
Janet Yu <janettyu99@gmail.com>
2022-12-29 17:30:49 -0800

    feat: create plan trip base page

---
+++ client/src/screens/PlanTrip/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PlanTripForm from './Forms/PlanTripForm';
+
+const PlanTripPage = (props: any) => {
+  return <PlanTripForm />;
+};
+
+export default PlanTripPage;
---
+++ client/src/stories/CreateTripForm.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import PlanTripComponent from '../screens/PlanTrip';
+
+export default {
+  title: 'PlanTrip',
+  component: PlanTripComponent,
+};
+
+export const PlanTrip = () => <PlanTripComponent />;

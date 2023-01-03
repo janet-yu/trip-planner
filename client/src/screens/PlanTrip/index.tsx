206dd80fcd4430cacfa6001703d7986cae4a933d
Janet Yu <janettyu99@gmail.com>
2023-01-02 19:55:59 -0800

    build: add yup

--- client/src/screens/PlanTrip/index.tsx
+++ client/src/screens/PlanTrip/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import PlanTripForm from './Forms/PlanTripForm';
+import styled from 'styled-components';
+import PageHeader from '../../components/PageHeader';
+
+const PageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  flex-direction: column;
+`;
+
+const PageInnerContainer = styled.div`
+  max-width: 728px;
+  width: 100%;
+`;
 
 const PlanTripPage = (props: any) => {
-  return <PlanTripForm />;
+  return (
+    <PageContainer>
+      <PageInnerContainer>
+        <PageHeader title="Plan a Trip" />
+        <PlanTripForm />
+      </PageInnerContainer>
+    </PageContainer>
+  );
 };
 
 export default PlanTripPage;

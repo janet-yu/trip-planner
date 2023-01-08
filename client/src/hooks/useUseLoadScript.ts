4964d596228c276d914cb48893c492c4d52d6dab
Janet Yu <janettyu99@gmail.com>
2023-01-07 21:18:29 -0800

    fix: define libraries for google maps api in useUseLoadScript file to avoid unnecessary re-renders

--- client/src/hooks/useUseLoadScript.ts
+++ client/src/hooks/useUseLoadScript.ts
@@ -1,6 +1,8 @@
 import { useLoadScript, LoadScriptProps } from '@react-google-maps/api';
 
-const useUseLoadScript = (libraries: LoadScriptProps['libraries']) => {
+const libraries: LoadScriptProps['libraries'] = ['places'];
+
+const useUseLoadScript = () => {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY || '', // @todo: figure out how to address dis
     libraries,

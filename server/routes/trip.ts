f68e5e5bb733fbcdeffa3cd179538f95e6331791
Janet Yu <janettyu99@gmail.com>
2023-01-14 15:52:49 -0800

    refactor: update trip patch operations and add get routes for itinerary and accommodations

--- server/routes/trip.ts
+++ server/routes/trip.ts
@@ -1,5 +1,12 @@
 import { Router } from 'express';
 import Trip from '../models/trip';
+import GoogleAPIService from '../lib/googleAPI';
+
+enum PATCH_OPERATIONS {
+  add = 'add',
+  remove = 'remove',
+  replace = 'replace',
+}
 
 const tripRouter = Router();
 
@@ -34,7 +41,14 @@ tripRouter.get('/:id', async (req, res) => {
 
   const trip = await Trip.findById(id);
 
-  res.status(200).send(trip);
+  const response = await GoogleAPIService.getPlaceDetails(
+    trip.placeReferenceId
+  );
+
+  res.status(200).send({
+    ...trip.toObject(),
+    ...response.data.result,
+  });
 });
 
 tripRouter.patch('/:id', async (req, res) => {
@@ -45,20 +59,81 @@ tripRouter.patch('/:id', async (req, res) => {
   // https://medium.com/@isuru89/a-better-way-to-implement-http-patch-operation-in-rest-apis-721396ac82bf
   const { op, field, value } = req.body;
 
-  if (op && op === 'add') {
+  if (op && op === PATCH_OPERATIONS.add) {
     // check for duplicates
     updates[field] = trip[field].concat(value);
-
-    console.log({ updates, field: trip[field], value });
   }
 
-  if (op && op === 'replace') {
+  if (op && op === PATCH_OPERATIONS.replace) {
     updates[field] = value;
+  }
+
+  if (op === PATCH_OPERATIONS.remove) {
+    if (trip[field].length !== undefined) {
+      // If the value is an object with an ID, this suggests
+      // we want to remove an object from an array
+      if (value.id) {
+        updates[field] = trip[field].filter((el) => {
+          return el._id.toString() !== value.id;
+        });
+      } else {
+        // Else, we want to remove a primitive
+        updates[field] = trip[field].filter((el) => el === value);
+      }
+    }
   }
 
   const updatedTrip = await Trip.findByIdAndUpdate(id, updates, { new: true });
 
   res.status(200).send(updatedTrip);
+});
+
+tripRouter.get('/:id/accommodations', async (req, res) => {
+  const { id } = req.params;
+  const trip = await Trip.findById(id);
+
+  let accommodations = [];
+
+  for (const acc of trip.accommodations) {
+    const response = await GoogleAPIService.getPlaceDetails(acc.referenceId);
+
+    accommodations.push({
+      // @ts-ignore
+      id: acc._id,
+      details: {
+        ...response.data.result,
+      },
+    });
+  }
+
+  res.status(200).json({
+    accommodations,
+  });
+});
+
+tripRouter.get('/:id/itinerary', async (req, res) => {
+  const { id } = req.params;
+  const trip = await Trip.findById(id);
+
+  let itinerary = [];
+
+  for (const activity of trip.itinerary) {
+    const response = await GoogleAPIService.getPlaceDetails(
+      activity.referenceId
+    );
+
+    itinerary.push({
+      // @ts-ignore
+      id: activity._id,
+      details: {
+        ...response.data.result,
+      },
+    });
+  }
+
+  res.status(200).json({
+    itinerary,
+  });
 });
 
 export default tripRouter;

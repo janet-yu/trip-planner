252e1d900647b5a7a70113d6a1c9dd7c6694da05
Janet Yu <janettyu99@gmail.com>
2023-02-05 09:46:59 -0800

    refactor: render App with necessary context providers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import './App.css';
-import { Theme } from './Theme';
 import LoginPage from './screens/LoginPage';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import PlanTripPage from './screens/PlanTrip';
+import TripPage from './screens/Trip';
+import { Theme } from './Theme';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index element={<LoginPage />} />
+      <Route path="plan-trip" element={<PlanTripPage />} />
+      <Route path="trip/:id" element={<TripPage />} />
+    </Route>
+  )
+);
+const queryClient = new QueryClient();
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <Theme>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </QueryClientProvider>
+    </Theme>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,31 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {
-  createRoutesFromElements,
-  createBrowserRouter,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
-import PlanTripPage from './screens/PlanTrip';
-import { Theme } from './Theme';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route index element={<App />} />
-      <Route path="plan-trip" element={<PlanTripPage />} />
-    </Route>
-  )
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
+// strict mode was the culprit for not allowing drag and drop???
 root.render(
-  <React.StrictMode>
-    <Theme>
-      <RouterProvider router={router} />
-    </Theme>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

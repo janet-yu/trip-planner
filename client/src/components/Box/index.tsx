fcc3a44bc7ca8d9d8f3a7f3016944e19872f58ad
Janet Yu <janettyu99@gmail.com>
2023-07-27 15:02:29 -0500

    refactor(components): update box props to accept more flexbox options

--- client/src/components/Box/index.tsx
+++ client/src/components/Box/index.tsx
@@ -1,11 +1,23 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { SpacingProps, generateSpacingProps } from '../utils';
 
 type BoxProps = {
   display?: string;
+  justifyContent?: string;
+  alignItems?: string;
+  flexDirection?: 'column' | 'row';
 } & SpacingProps;
 
 const Box = styled.div<BoxProps>`
+  display: ${(props) => (props.display ? props.display : 'block')};
+  ${(props) =>
+    props.display === 'flex' &&
+    css`
+      display: flex;
+      ${props.justifyContent && `justify-content: ${props.justifyContent};`}
+      ${props.alignItems && `align-items: ${props.alignItems};`}
+      ${props.flexDirection && `flex-direction: ${props.flexDirection};`}
+    `}
   ${(props) => generateSpacingProps(props)}
 `;
 

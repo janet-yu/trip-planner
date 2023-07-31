e4ae90e735ba1578377e846419746f35f9a1d09c
Janet Yu <janettyu99@gmail.com>
2023-07-31 13:20:31 -0500

    refactor: add padding props to Box component

--- client/src/components/utils.ts
+++ client/src/components/utils.ts
@@ -8,6 +8,14 @@ export const generateSpacingProps = (props: Partial<SpacingProps>) => {
   ${props.mx && `margin: 0px ${props.mx}px;`}
   ${props.mLeft && `margin: 0px 0px 0px ${props.mLeft}px;`}
   ${props.mRight && `margin: 0px ${props.mRight}px 0px 0px;`}
+
+  ${props.p && `padding: ${props.p}px;`}
+  ${props.px && `padding: 0px ${props.py}px;`}
+  ${props.py && `padding: ${props.py}px 0px;`}
+  ${props.pTop && `padding: ${props.pTop}px 0px 0px 0px;`}
+  ${props.pBottom && `padding: 0px 0px ${props.pBottom}px 0px;`}
+  ${props.pRight && `padding: 0px ${props.pRight}px 0px 0px;`}
+  ${props.pLeft && `padding: 0px 0px 0px ${props.pLeft}px;`}
   `;
 };
 
@@ -18,4 +26,11 @@ export type SpacingProps = {
   mBottom?: number;
   mLeft?: number;
   mRight?: number;
+  p?: number;
+  py?: number;
+  px?: number;
+  pTop?: number;
+  pBottom?: number;
+  pLeft?: number;
+  pRight?: number;
 };

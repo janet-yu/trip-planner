e33ad6d87585f3231b914140d4c7ceefbc8813f7
Janet Yu <janettyu99@gmail.com>
2023-08-02 12:50:41 -0500

    refactor: make modal popup responsive

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { device } from '../../utils/mediaQueries';
 
 const ModalContainer = styled.div`
   width: 100%;
@@ -20,14 +21,23 @@ const Overlay = styled.div`
 
 const ModalPopup = styled.div`
   background: #fff;
-  border-radius: 16px;
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   padding: 24px;
-  width: 40%;
-  max-width: 500px;
+  bottom: 0;
+  width: 100%;
+  border-radius: 16px 16px 0 0;
+  height: 50vh;
+  overflow-y: auto;
+
+  @media ${device.tablet} {
+    border-radius: 16px;
+    width: 40%;
+    max-width: 500px;
+    top: 50%;
+    left: 50%;
+    bottom: auto;
+    height: auto;
+    transform: translate(-50%, -50%);
 `;
 
 const Modal = ({
